a265566e9a73940b69b94dc6e5a72da4fe720bdd
Hashem Abou Ahmad <hashemaa.ca@gmail.com>
2023-09-18 21:57:39 -0400

    fix deployment 3

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,40 +1,40 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import OpenAI from "openai";
+// import OpenAI from "openai";
 
 dotenv.config();
 
 const router = express.Router();
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" });
 });
 
-router.route("/").post(async (req, res) => {
-  try {
-    const { prompt } = req.body;
+// router.route("/").post(async (req, res) => {
+//   try {
+//     const { prompt } = req.body;
 
-    const response = await openai.images.generate({
-      prompt,
-      n: 1,
-      size: "1024x1024",
-      response_format: "b64_json",
-    });
+//     const response = await openai.images.generate({
+//       prompt,
+//       n: 1,
+//       size: "1024x1024",
+//       response_format: "b64_json",
+//     });
 
-    const image = response.data.data[0].b64_json;
+//     const image = response.data.data[0].b64_json;
 
-    res.status(200).json({ photo: image });
-  } catch (error) {
-    if (error.code == "billing_hard_limit_reached") {
-      //no available credits
-    }
-    console.error(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
+//     res.status(200).json({ photo: image });
+//   } catch (error) {
+//     if (error.code == "billing_hard_limit_reached") {
+//       //no available credits
+//     }
+//     console.error(error);
+//     res.status(500).json({ message: "Something went wrong" });
+//   }
+// });
 
 export default router;
